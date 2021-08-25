b4e90eb39756a2ecc2086ef2cadce7da60892ae5
benstutch <87542874+benstutch@users.noreply.github.com>
2021-08-25 10:24:15 +0100

    Add files via upload

---
+++ TBS testers.R
@@ -0,0 +1,227 @@
+
+
+
+
+
+  centre_tbs = c(6,6,6)
+  rregistration_tbs = c(1,1,1)
+  rinpatient_tbs = c(0,0,0)
+  rwaiting_time_tbs = c(0,0,0) 
+  rage_tbs = c(66,66,66)
+  rgender_tbs = c(0,0,0)
+  rdisease_primary_tbs = c(9,9,9)
+  rdisease_secondary_tbs = c(10,1,1)
+  rdisease_tertiary_tbs = c(10,10,10)
+  previous_tx_tbs = c(0,0,0)
+  rprevious_surgery_tbs = c(0,0,0)
+  rbilirubin_tbs = c(49,49,49)
+  rinr_tbs = c(1.4,1.4,1.4)
+  rcreatinine_tbs = c(70,70,70)
+  rrenal_tbs = c(0,0,0)
+  rsodium_tbs = c(135,135,135)
+  rpotassium_tbs = c(4.5,4.5,4.5)
+  ralbumin_tbs = c(30,30,30)
+  rencephalopathy_tbs = c(0,0,0) 
+  rascites_tbs = c(0,0,0)
+  rdiabetes_tbs = c(0,0,0)
+  rmax_afp_tbs = c(0,5,5)
+  rtumour_number_tbs = c(0,1,1)
+  rmax_tumour_size_tbs = c(0,2,4.9)
+  dage_tbs = c(52,52,52)
+  dcause_tbs = c(1,1,1)
+  dbmi_tbs = c(25,25,25)
+  ddiabetes_tbs = c(0,0,0)
+  dtype_tbs = c(0,0,0)
+  splittable_tbs = c(0,0,0)
+  bloodgroup_compatible_tbs = c(0,0,0)
+
+
+
+tibble (
+  
+  fn_tbs(centre_tbs,
+                      rregistration_tbs,
+                      rinpatient_tbs,
+                      rwaiting_time_tbs,
+                      rage_tbs,
+                      rgender_tbs,
+                      rdisease_primary_tbs, 
+                      rdisease_secondary_tbs,
+                      rdisease_tertiary_tbs,
+                      previous_tx_tbs,
+                      rprevious_surgery_tbs,
+                      rbilirubin_tbs,
+                      rinr_tbs,
+                      rcreatinine_tbs,
+                      rrenal_tbs,
+                      rsodium_tbs,
+                      rpotassium_tbs,
+                      ralbumin_tbs,
+                      rencephalopathy_tbs,
+                      rascites_tbs,
+                      rdiabetes_tbs,
+                      rmax_afp_tbs,
+                      rtumour_number_tbs,
+                      rmax_tumour_size_tbs,
+                      dage_tbs,
+                      dcause_tbs,
+                      dbmi_tbs,
+                      ddiabetes_tbs,
+                      dtype_tbs,
+                      splittable_tbs,
+                      bloodgroup_compatible_tbs) )
+
+base_data
+
+
+base_data
+
+fn_tbs ( centre_tbs = c(6,6,6),
+         rregistration_tbs = c(1,1,1),
+         rinpatient_tbs = c(0,0,0),
+         rwaiting_time_tbs = c(0,0,0), 
+         rage_tbs = c(66,66,66),
+         rgender_tbs = c(0,0,0),
+         rdisease_primary_tbs = c(9,9,9),
+         rdisease_secondary_tbs = c(10,1,1),
+         rdisease_tertiary_tbs = c(10,10,10) ,
+         previous_tx_tbs = c(0,0,0),
+         rprevious_surgery_tbs = c(0,0,0),
+         rbilirubin_tbs = c(49,49,49),
+         rinr_tbs = c(1.4,1.4,1.4),
+         rcreatinine_tbs = c(70,70,70),
+         rrenal_tbs = c(0,0,0),
+         rsodium_tbs = c(135,135,135),
+         rpotassium_tbs = c(4.5,4.5,4.5),
+         ralbumin_tbs = c(30,30,30),
+         rencephalopathy_tbs = c(0,0,0),
+         rascites_tbs = c(0,0,0),
+         rdiabetes_tbs = c(0,0,0),
+         rmax_afp_tbs = c(0,5,5),
+         rtumour_number_tbs = c(0,1,1),
+         rmax_tumour_size_tbs = c(0,2,4.9),
+         dage_tbs = c(52,52,52),
+         dcause_tbs = c(1,1,1),
+         dbmi_tbs = c(25,25,25),
+         ddiabetes_tbs = c(0,0,0) ,
+         dtype_tbs = c(0,0,0),
+         splittable_tbs = c(0,0,0),
+         bloodgroup_compatible_tbs = c(0,0,0),
+         
+         include_table = FALSE)
+
+
+
+
+
+
+
+function(centre_tbs,
+         rregistration_tbs,
+         rinpatient_tbs,
+         rwaiting_time_tbs,
+         rage_tbs,
+         rgender_tbs,
+         rdisease_primary_tbs, 
+         rdisease_secondary_tbs,
+         rdisease_tertiary_tbs,
+         previous_tx_tbs,
+         rprevious_surgery_tbs,
+         rbilirubin_tbs,
+         rinr_tbs,
+         rcreatinine_tbs,
+         rrenal_tbs,
+         rsodium_tbs,
+         rpotassium_tbs,
+         ralbumin_tbs,
+         rencephalopathy_tbs,
+         rascites_tbs,
+         rdiabetes_tbs,
+         rmax_afp_tbs,
+         rtumour_number_tbs,
+         rmax_tumour_size_tbs,
+         dage_tbs,
+         dcause_tbs,
+         dbmi_tbs,
+         ddiabetes_tbs,
+         dtype_tbs,
+         splittable_tbs,
+         bloodgroup_compatible_tbs,
+         
+         include_table = FALSE
+)
+
+tibble (
+  
+  fn_tbs(  centre_tbs = 6,
+         rregistration_tbs = 1, 
+         rinpatient_tbs = 0,
+         rwaiting_time_tbs = 0 ,
+         rage_tbs = 66,
+         rgender_tbs = 0,
+         rdisease_primary_tbs = 1, 
+         rdisease_secondary_tbs = 10, 
+         rdisease_tertiary_tbs = 10, 
+         previous_tx_tbs = 0,
+         rprevious_surgery_tbs = 0,
+         rbilirubin_tbs = 49,
+         rinr_tbs = 1.4,
+         rcreatinine_tbs = 70,
+         rrenal_tbs = 0,
+         rsodium_tbs = 135,
+         rpotassium_tbs = 4.5,
+         ralbumin_tbs = 30,
+         rencephalopathy_tbs = 0,
+         rascites_tbs = 0, 
+         rdiabetes_tbs = 0, 
+         rmax_afp_tbs = 5, 
+         rtumour_number_tbs = 1,
+         rmax_tumour_size_tbs = 2,
+         dage_tbs = 52,
+         dcause_tbs = 1,
+         dbmi_tbs = 25,
+         ddiabetes_tbs = 0, 
+         dtype_tbs = 0,
+         splittable_tbs = 0,
+         bloodgroup_compatible_tbs = 0, 
+         include_table = FALSE)
+        
+        )
+
+
+
+
+
+tibble ( fn_tbs(  centre_tbs = c(6,6),
+         rregistration_tbs = c(1,1), 
+         rinpatient_tbs = c(0,0),
+         rwaiting_time_tbs = c(0,0) ,
+         rage_tbs = c(66,66),
+         rgender_tbs = c(0,0),
+         rdisease_primary_tbs = c(1,1), 
+         rdisease_secondary_tbs = c(10,10), 
+         rdisease_tertiary_tbs = c(10,10), 
+         previous_tx_tbs = c(0,0),
+         rprevious_surgery_tbs = c(0,0),
+         rbilirubin_tbs = c(49,49),
+         rinr_tbs = c(1.4,1.4),
+         rcreatinine_tbs = c(70,70),
+         rrenal_tbs = c(0,0),
+         rsodium_tbs = c(135,135),
+         rpotassium_tbs = c(4.5,4.5),
+         ralbumin_tbs = c(30,30),
+         rencephalopathy_tbs = c(0,0),
+         rascites_tbs = c(0,0), 
+         rdiabetes_tbs = c(0,0), 
+         rmax_afp_tbs = c(5,5), 
+         rtumour_number_tbs = c(1,1),
+         rmax_tumour_size_tbs = c(2,2),
+         dage_tbs = c(52,52),
+         dcause_tbs = c(1,1),
+         dbmi_tbs = c(25,25),
+         ddiabetes_tbs = c(0,0), 
+         dtype_tbs = c(0,0),
+         splittable_tbs = c(0,0),
+         bloodgroup_compatible_tbs = c(0,0), 
+         include_table = FALSE))
+
